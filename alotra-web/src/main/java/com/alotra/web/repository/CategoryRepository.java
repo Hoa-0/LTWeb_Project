4ae103f38e2a5ef0a2a0c49b4c4f49b7226c8a94
Tainguyen273 <tai3717273@gmail.com>
2025-10-26 23:06:55 +0700

    Xoa toan bo noi loi main

--- alotra-web/src/main/java/com/alotra/web/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alotra.web.repository;
-
-import com.alotra.web.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- alotra-web/src/main/java/com/alotra/web/repository/OrderDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alotra.web.repository;
-
-import com.alotra.web.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-}
