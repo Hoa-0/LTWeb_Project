3299a0e1c8616939edb70dc830aa163d3a9d68f9
Tainguyen273 <tai3717273@gmail.com>
2025-10-27 18:40:26 +0700

    Merge main and resolved conflicts

--- alotra-web/src/main/java/com/alotra/web/enums/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.alotra.web.enums;
-
-public enum OrderStatus {
-    PENDING,        // Mới đặt
-    CONFIRMED,      // Đã xác nhận
-    SHIPPING,       // Đang giao
-    COMPLETED,      // Đã giao thành công
-    CANCELLED       // Đã hủy
-}
